c45fb978c7d45752ef6246e63cca215e087dc9c2
tsguru721 <lovetom.pro@gmail.com>
2024-01-19 03:34:23 +0200

    use warn

--- src/app.service.ts
+++ src/app.service.ts
@@ -195,7 +195,7 @@ export class AppService {
       let endTime = Date.now(); // End time recording
       let renderingTime = endTime - startTime; // Calculate rendering time
       
-      this.logger.log(`WAV file rendering time: ${renderingTime} ms`); // Log rendering time
+      this.logger.warn(`WAV file rendering time: ${renderingTime} ms`); // Log rendering time
       return new StreamableFile(file);
     } else {
       // If file generation failed, send a warning- this isn't supposed to happen
